dc168fcd9be52e2842d45057f791132e10877f82
Matteo <etu54891@gmail.com>
2025-04-01 23:23:07 +0200

    Update UI

--- src/userInterface/CommandPanel.java
+++ src/userInterface/CommandPanel.java
@@ -3,5 +3,5 @@ package userInterface;
 import javax.swing.*;
 
 public class CommandPanel extends JPanel {
-
+    // Créer un seul panel et tout faire dans un ou créer deux panel différent et le met dans un ou créer deux panels différents et les mettre dans le JFrame direct (Perso je pense tout faire dans un comme chungly);
 }
