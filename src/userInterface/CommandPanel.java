9eb92b4061930a86c485186750c592800921f744
Chungly <tengchungly@gmail.com>
2025-04-06 00:31:04 +0200

    Ajout de listener comboBox et class OrderDBAccess

--- src/userInterface/CommandPanel.java
+++ src/userInterface/CommandPanel.java
@@ -5,16 +5,17 @@ import model.User;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.ArrayList;
 
 public class CommandPanel extends JPanel {
-    private JLabel userLabel;
     private ApplicationController controller;
 
-    //private ArrayList<User> users;
-    //private JList usersList;
-    private JComboBox<User> usersComboList;
+    private ArrayList<User> users;
     private JPanel allUsersPanel;
+    private JLabel userLabel;
+    private JComboBox<User> usersComboList;
 
     // CONSTRUCTORS
     public CommandPanel() {
@@ -22,29 +23,18 @@ public class CommandPanel extends JPanel {
         setController(new ApplicationController());
 
         try {
-            // SI ON A BESOIN D'UNE JLIST
-            //users = controller.getAllUsers();
-            //DefaultListModel<User> listModel = new DefaultListModel<>();
-            //for (User user : users) {
-            //    listModel.addElement(user);
-            //}
-            //usersList = new JList<>(listModel); // toArray => transforme l'objet en array
-            //usersList.setVisibleRowCount(1);
-            //usersList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-            //JScrollPane scrollPane = new JScrollPane(usersList);
-            //this.add(scrollPane, BorderLayout.CENTER);
-
+            userLabel = new JLabel("Utilisateur : ");
             allUsersPanel = new JPanel(new FlowLayout());
+            users = controller.getAllUsers();
 
-            ArrayList<User> users = controller.getAllUsers();
             usersComboList = new JComboBox<>(users.toArray(new User[0]));
             usersComboList.setPreferredSize(new Dimension(200, 25));
-
-            userLabel = new JLabel("Utilisateur : ");
+                //ComboBox Listener
+            ComboBoxListener comboListener = new ComboBoxListener();
+            usersComboList.addItemListener(comboListener);
 
             allUsersPanel.add(userLabel);
             allUsersPanel.add(usersComboList);
-
             this.add(allUsersPanel, BorderLayout.NORTH);
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, e.getMessage(), "Erreur", JOptionPane.ERROR_MESSAGE);
@@ -53,5 +43,12 @@ public class CommandPanel extends JPanel {
     // SETTERS
     public void setController(ApplicationController controller) {
         this.controller = controller;
+    }
+
+    // SOUS-CLASSE LISTENERS
+    private class ComboBoxListener implements ItemListener {
+        public void itemStateChanged(ItemEvent e) {
+
+        }
     }
 }
