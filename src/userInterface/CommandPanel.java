ec2ee9dc630a8095188ff2f4469317249e027b02
saccmatteo <etu54891@henallux.be>
2025-04-07 22:41:52 +0200

    Ajout affichage JList, boutons, style affichage

--- src/userInterface/CommandPanel.java
+++ src/userInterface/CommandPanel.java
@@ -7,7 +7,11 @@ import model.User;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.util.ArrayList;
@@ -20,7 +24,7 @@ public class CommandPanel extends JPanel {
     private ArrayList<Order> orders;
 
     private JPanel allUsersPanel, buttonsPanel, allOrdersPanel;
-    private JButton removeCommand, createCommand, updateCommand;
+    private JButton removeCommand, createCommand, updateCommand, validateCommand, clearSelection;
     private JLabel userLabel;
 
     private JComboBox<User> usersComboList;
@@ -33,18 +37,21 @@ public class CommandPanel extends JPanel {
 
         try {
             // User choice
-            setUserController(new UserController());
-            userLabel = new JLabel("Utilisateur : ");
             allUsersPanel = new JPanel(new FlowLayout());
+            setUserController(new UserController());
+            // Params Label combolist
+            userLabel = new JLabel("Utilisateur : ");
+            userLabel.setFont(new Font("Arial", Font.BOLD, 30));
             allUsersPanel.setBorder(new EmptyBorder(40, 0, 40, 0));
             users = userController.getAllUsers();
-
+            // ComboBox params
             usersComboList = new JComboBox<>(users.toArray(new User[0]));
-            usersComboList.setPreferredSize(new Dimension(300, 30));
-                //ComboBox Listener
+            usersComboList.setPreferredSize(new Dimension(300, 40));
+            usersComboList.setFont(new Font("Arial", Font.BOLD, 20));
+            // ComboBox Listener
             ComboBoxListener comboListener = new ComboBoxListener();
             usersComboList.addItemListener(comboListener);
-
+            // Ajout + params au Panel
             allUsersPanel.add(userLabel);
             allUsersPanel.add(usersComboList);
             this.add(allUsersPanel, BorderLayout.NORTH);
@@ -52,29 +59,48 @@ public class CommandPanel extends JPanel {
             // Orders buttons
             buttonsPanel = new JPanel(new FlowLayout());
             buttonsPanel.setBorder(new EmptyBorder(0,0, 40, 0));
-
             removeCommand = new JButton("Supprimer commande");
             createCommand = new JButton("Prendre commande");
             updateCommand = new JButton("Modifier commande");
+            validateCommand = new JButton("Cloturer commande");
 
-            buttonsPanel.add(updateCommand);
             buttonsPanel.add(createCommand);
+            // LISTENERS + Ajout au Panel
+            updateCommand.setEnabled(false);
+            buttonsPanel.add(updateCommand);
+
+            removeCommand.setEnabled(false);
             buttonsPanel.add(removeCommand);
+
+            validateCommand.setEnabled(false);
+            buttonsPanel.add(validateCommand);
             this.add(buttonsPanel);
 
             // Orders list
             allOrdersPanel = new JPanel(new FlowLayout());
             setOrderController(new OrderController());
-
             this.orders = orderController.getAllOrders();
             ordersList = new JList<>(orders.toArray(new Order[0])); // Conversion en tableau
+            // Params JList
             ordersList.setVisibleRowCount(5);
             ordersList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
             ordersList.clearSelection();
-
+            // Params affichage JList
+            ordersList.setFont(new Font("Arial", Font.PLAIN, 36)); // Agrandir la taille de la police
+            ordersList.setFixedCellHeight(60); // agrandir les lignes de la JList
+            ordersList.setBorder(new EmptyBorder(10, 10, 10, 10));
+            ordersList.addListSelectionListener(new JListListener());
+            // ScrollPane
             ordersScrollPane = new JScrollPane(ordersList);
+            ordersScrollPane.setPreferredSize(new Dimension(1000, 350));
+            // Ajoute + params au Panel
             allOrdersPanel.add(ordersScrollPane);
             allOrdersPanel.setBorder(new EmptyBorder(0,0, 40, 0));
+            allOrdersPanel.setPreferredSize(new Dimension(1000, 600));
+            // Ajout clearButtonSelection + params affichage du bouton
+            clearSelection = new JButton("Réinitialiser le choix");
+            clearSelection.addActionListener(new ClearSelectionListener());
+            allOrdersPanel.add(clearSelection);
             this.add(allOrdersPanel, BorderLayout.SOUTH);
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, e.getMessage(), "Erreur", JOptionPane.ERROR_MESSAGE);
@@ -93,6 +119,23 @@ public class CommandPanel extends JPanel {
     private class ComboBoxListener implements ItemListener {
         public void itemStateChanged(ItemEvent e) {
 
+        }
+    }
+
+    private class JListListener implements ListSelectionListener {
+        public void valueChanged(ListSelectionEvent e) {
+            updateCommand.setEnabled(true);
+            removeCommand.setEnabled(true);
+            validateCommand.setEnabled(true);
+        }
+    }
+
+    private class ClearSelectionListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            ordersList.clearSelection();
+            updateCommand.setEnabled(false);
+            removeCommand.setEnabled(false);
+            validateCommand.setEnabled(false);
         }
     }
 }
