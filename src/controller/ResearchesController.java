9ab8b8d8936f5755bbc292416a6d3d62329808f9
Matteo <etu54891@gmail.com>
2025-05-16 21:53:55 +0200

    regroupement des controllers recherches en un seul controller

---
+++ src/controller/ResearchesController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import businessLogic.AlcoholicDrinkManager;
+import businessLogic.OrderInfosManager;
+import businessLogic.ProductUnderThresholdManager;
+import model.AlcoholicDrinksInfos;
+import model.OrderInfos;
+import model.ProductsUnderThreshold;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class ResearchesController {
+    AlcoholicDrinkManager alcoholicDrinkManager;
+    ProductUnderThresholdManager productUnderThresholdManager;
+    OrderInfosManager orderInfosManager;
+
+    public ResearchesController() {
+        this.alcoholicDrinkManager = new AlcoholicDrinkManager();
+        this.productUnderThresholdManager = new ProductUnderThresholdManager();
+        this.orderInfosManager = new OrderInfosManager();
+    }
+
+    public ArrayList<AlcoholicDrinksInfos> getAlcDrinksBeforeDate(LocalDate date) {
+        return alcoholicDrinkManager.getAlcDrinksBeforeDate(date);
+    }
+
+    public ArrayList<ProductsUnderThreshold> getProductsUnderThreshold(int threshold) {
+        return productUnderThresholdManager.getAllProductsUnderThreshold(threshold);
+    }
+
+    public ArrayList<OrderInfos> getAllOrdersInfos(int productId) {
+        return orderInfosManager.getAllOrdersInfos(productId);
+    }
+}
--- src/userInterface/ResearchPanel.java
+++ src/userInterface/ResearchPanel.java
@@ -1,32 +1,46 @@
 package userInterface;
 
-import controller.AlcoholicDrinkController;
+import controller.ResearchesController;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.time.LocalDate;
 
 public class ResearchPanel extends JPanel {
-    private AlcoholicDrinkController controller;
+    private String [] researches = {"Boissons alcoolisées", "Produit en dessous du seuil", "Informations commande d'un produit"};
+    private JComboBox research;
+    private ResearchesController controller;
     private JTable table;
     private JScrollPane scrollPane;
     private AlcoholicDrinksModel model;
 
+
     public ResearchPanel() {
-        setController(new AlcoholicDrinkController());
-        LocalDate date = LocalDate.of(2001, 1, 1);
-        model = new AlcoholicDrinksModel(controller.getAlcDrinksBeforeDate(date));
+
+        research = new JComboBox(researches);
+        research.addActionListener(new researchListener());
+        research.setSelectedIndex(-1);
+        this.add(research);
+        //model = new AlcoholicDrinksModel(controller.getAlcDrinksBeforeDate());
         table = new JTable(model);
         table.setEnabled(false);
         table.setRowHeight(50);
         scrollPane = new JScrollPane(table);
         scrollPane.setPreferredSize(new Dimension(1000, 800));
-        this.add(scrollPane);
+        //this.add(scrollPane);
         this.setVisible(true);
     }
 
-    public void setController(AlcoholicDrinkController controller) {
-        this.controller = controller;
+    private class researchListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String selectedItem = (String)research.getSelectedItem();
+            if (selectedItem.equals("Boissons alcoolisées")) {
+                JSpinner spinner = new JSpinner();
+                add(spinner);
+            }
+        }
     }
 }
